2fc04d6d99fd47999d714e1a76d32405d0d382d7
Osama Tbaileh <osama.tb02@gmail.com>
2022-10-24 11:50:30 +0300

    "First Java Program" Assignment

--- Java Assignments/As1FirstJavaProgram/FirstJavaProgram.java
+++ Java Assignments/As1FirstJavaProgram/FirstJavaProgram.java
@@ -1,7 +1,7 @@
 public class FirstJavaProgram {
     public static void main (String[] args){
-        System.out.println("Osama Tbaileh");
-        System.out.println("22 years");
-        System.out.println("Nablus");
+        System.out.println("My name is Osama Tbaileh");
+        System.out.println("I am 22 years old");
+        System.out.println("My home town is Nablus, Palestine");
     }
 }
