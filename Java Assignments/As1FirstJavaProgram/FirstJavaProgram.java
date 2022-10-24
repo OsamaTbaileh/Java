47d18622eb4d967850e38a5697d33e867ab35221
Osama Tbaileh <osama.tb02@gmail.com>
2022-10-24 11:45:44 +0300

    "First Java Program" Assignment

---
+++ Java Assignments/As1FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main (String[] args){
+        System.out.println("Osama Tbaileh");
+        System.out.println("22 years");
+        System.out.println("Nablus");
+    }
+}
