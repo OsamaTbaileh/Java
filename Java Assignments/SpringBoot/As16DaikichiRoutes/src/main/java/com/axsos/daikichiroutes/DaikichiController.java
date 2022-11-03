41c7c04e1de12d2411f7adafd7c5a5c3627dd6be
Osama Tbaileh <osama.tb02@gmail.com>
2022-11-03 15:13:46 +0200

    update

--- Java Assignments/SpringBoot/As16DaikichiRoutes/src/main/java/com/axsos/daikichiroutes/DaikichiController.java
+++ Java Assignments/SpringBoot/As16DaikichiRoutes/src/main/java/com/axsos/daikichiroutes/DaikichiController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/daikichi")
-public class DaikichiRoutes {
+public class DaikichiController {
 		
 	@RequestMapping("")
 	public String viewMain() {
