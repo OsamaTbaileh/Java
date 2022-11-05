5e4c73578e3eda267fdc8f930dc8f0b684b4e929
Osama Tbaileh <osama.tb02@gmail.com>
2022-11-05 13:48:17 +0200

    update

--- Java Assignments/SpringBoot/As20DisplayDate/src/main/resources/static/js/date.js
+++ Java Assignments/SpringBoot/As20DisplayDate/src/main/resources/static/js/date.js
@@ -1,1 +1,11 @@
-alert("This is the date template");
+// two ways to meke the same alert:
+
+
+//alert("This is the date template");
+
+
+if (confirm("This is the " + document.title.toLocaleLowerCase() +" template")) {
+  txt = "You pressed OK";
+} else {
+  txt = "You pressed Cancel";
+}
--- Java Assignments/SpringBoot/As20DisplayDate/src/main/resources/static/js/time.js
+++ Java Assignments/SpringBoot/As20DisplayDate/src/main/resources/static/js/time.js
@@ -1,1 +1,10 @@
+// two ways to meke the same alert:
+
 alert("This is the time template");
+
+
+//if (confirm("This is the " + document.title.toLocaleLowerCase() +" template")) {
+//  txt = "You pressed OK";
+//} else {
+//  txt = "You pressed Cancel";
+//}
